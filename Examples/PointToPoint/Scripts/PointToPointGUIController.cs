ecc9bcee5cceef14377cf63675f72e136753fe5f
Adam J. Mendoza <adammendoza@hotmail.com>
2016-12-04 10:38:44 -0800

    Copied code to PointToPointController and commented out one of them to fix the duplicate class error

--- Examples/PointToPoint/Scripts/PointToPointGUIController.cs
+++ Examples/PointToPoint/Scripts/PointToPointGUIController.cs
@@ -32,7 +32,8 @@ public class PointToPointGUIController : MonoBehaviour, ITangoDepth
     public const float UI_LABEL_START_X = 15.0f;
     public const float UI_LABEL_START_Y = 15.0f;
     public const float UI_LABEL_SIZE_X = 1920.0f;
-    public const float UI_LABEL_SIZE_Y = 35.0f;
+    public const float UI_LABEL_SIZE_Y = 500.0f;
+    private Vector3 startPos;
 
     /// <summary>
     /// The point cloud object in the scene.
@@ -60,6 +61,14 @@ public class PointToPointGUIController : MonoBehaviour, ITangoDepth
     /// </summary>
     private Vector3 m_startPoint;
 
+    private Vector3 m_point0;
+    private Vector3 m_point1;
+    private Vector3 m_point2;
+    private Vector3 m_point3;
+    private Vector3 m_point4;
+
+    private int screenTaps = 0;
+
     /// <summary>
     /// The newer of the two points to measure.
     /// </summary>
@@ -68,36 +77,19 @@ public class PointToPointGUIController : MonoBehaviour, ITangoDepth
     /// <summary>
     /// The distance between the two selected points.
     /// </summary>
-    private float m_distance;
+    private float m_distance01;
+    private float m_distance12;
+    private float m_distance23;
+    private float m_distance34;
+
 
     /// <summary>
     /// The text to display the distance.
     /// </summary>
-    private string m_distanceText;
-
-	private bool hasMouseInput = false;
-	private List<Vector3> linePoints = new List<Vector3>();
-	private int lineCount = 0;
-
-	private Vector3 lastPos = Vector3.one * float.MaxValue;
-	private float threshold = 0.001f;
-
-	private struct Line {
-		Vector3 startPoint;
-		Vector3 endPoint;
-	}
-
-	private bool enableLineDrawing = true;
-
-	/// <summary>
-	/// Reference to the TangoPointCloudFloor in the scene.
-	/// </summary>
-	public TangoPointCloudFloor m_pointCloudFloor;
-
-	/// <summary>
-	/// If <c>true</c>, floor finding is in progress.
-	/// </summary>
-	private bool m_findingFloor = false;
+    private string m_distanceText01;
+    private string m_distanceText12;
+    private string m_distanceText23;
+    private string m_distanceText34;
 
     /// <summary>
     /// Start this instance.
@@ -105,12 +97,8 @@ public class PointToPointGUIController : MonoBehaviour, ITangoDepth
     public void Start()
     {
         m_tangoApplication = FindObjectOfType<TangoApplication>();
+
         m_tangoApplication.Register(this);
-		m_pointCloud = FindObjectOfType<TangoPointCloud>();
-		m_pointCloudFloor = FindObjectOfType<TangoPointCloudFloor>();
-
-		//m_tangoApplication.SetDepthCameraRate(TangoEnums.TangoDepthCameraRate.MAXIMUM);
-
     }
 
     /// <summary>
@@ -127,16 +115,35 @@ public class PointToPointGUIController : MonoBehaviour, ITangoDepth
     public void Update()
     {
         // Distance was found.
-        m_distanceText = "Distance is " + m_distance + " meters.";
+
+        if (screenTaps == 2) {
+            m_distanceText01 = "Length is:" + m_distance01;  //"01 Distance is " + m_distance01 + " meters.";
+            m_distanceText12 = "";
+            m_distanceText23 = "";
+
+        } else if (screenTaps == 3) {
+            m_distanceText01 = "Length is:" + m_distance01;  //"01 Distance is " + m_distance01 + " meters.";
+            m_distanceText12 = "Width is:" +  m_distance12;  //"12 Distance is " + m_distance12 + " meters.";
+            m_distanceText23 = "";
+
+        } else if (screenTaps > 3) {
+
+            m_distanceText01 = "Length is:" + m_distance01;  //"01 Distance is " + m_distance01 + " meters.";
+            m_distanceText12 = "Width is:" +  m_distance12;
+            m_distanceText23 = "Height is:"  + m_distance23;
 
 
-		_RenderLine();
+
+
+        }
+
+
+
+        // _RenderLine();
 
         if (Input.GetMouseButtonDown(0))
         {
-			if (enableLineDrawing) {
-				StartCoroutine (_WaitForDepth (Input.mousePosition));
-			}
+            StartCoroutine(_WaitForDepth(Input.mousePosition));
         }
 
         if (Input.GetKey(KeyCode.Escape))
@@ -157,10 +164,14 @@ public class PointToPointGUIController : MonoBehaviour, ITangoDepth
         {
             GUI.color = Color.black;
             GUI.Label(new Rect(UI_LABEL_START_X,
-                               UI_LABEL_START_Y,
-                               UI_LABEL_SIZE_X,
-                               UI_LABEL_SIZE_Y),
-                      "<size=25>" + m_distanceText + "</size>");
+                UI_LABEL_START_Y,
+                UI_LABEL_SIZE_X,
+                UI_LABEL_SIZE_Y),
+                "<size=25>" + m_distanceText01 +  "</size>"+ "\n" +
+                "<size=25>" + m_distanceText12 +  "</size>"+ "\n" +
+                "<size=25>" + m_distanceText23 +  "</size>"+ "\n"
+                //  "<size=25>" + m_distanceText34 +  "</size>"
+            );
         }
     }
 
@@ -184,7 +195,6 @@ public class PointToPointGUIController : MonoBehaviour, ITangoDepth
     {
         m_tangoApplication.SetDepthCameraRate(
             TangoEnums.TangoDepthCameraRate.DISABLED);
-		//m_tangoApplication.SetDepthCameraRate(TangoEnums.TangoDepthCameraRate.MAXIMUM);
     }
 
     /// <summary>
@@ -199,46 +209,12 @@ public class PointToPointGUIController : MonoBehaviour, ITangoDepth
     /// </summary>
     private void _RenderLine()
     {
-//		m_lineRenderer.SetPosition(0, Input.mousePosition);
-//		Vector3 point = new Vector3 (Input.mousePosition.x,Input.mousePosition.y+1);
-//		m_lineRenderer.SetPosition (1, point);
-
-		if (linePoints.Count == 4 && !enableLineDrawing) {
-			flattenLines ();
-		}
-
-		m_lineRenderer.SetVertexCount (linePoints.Count);
-		for (int i = 0; i < linePoints.Count; i++) {
-			m_lineRenderer.SetPosition (i, linePoints [i]);
-		}
-
-		if (linePoints.Count == 4) {
-			m_lineRenderer.SetVertexCount(5);
-			m_lineRenderer.SetPosition (4, linePoints [0]);
-		}
+        //  m_lineRenderer.SetPosition(0, m_point0);
+        m_lineRenderer.SetPosition(1, m_point1);
+        m_lineRenderer.SetPosition(2, m_point2);
+        m_lineRenderer.SetPosition(3, m_point3);
+        m_lineRenderer.SetPosition(4, m_point4);
     }
-
-	private void flattenLines() {
-		if (!m_findingFloor) {
-				m_findingFloor = true;
-
-				m_tangoApplication.SetDepthCameraRate(
-					TangoEnums.TangoDepthCameraRate.MAXIMUM);
-				m_pointCloud.FindFloor();
-		} else {
-			// flatten/normalize lines to the floor.
-			if (m_pointCloud.m_floorFound) {
-				m_findingFloor = false;
-					for (int i = 0; i < linePoints.Count; i++) {
-						Vector3 tempFlattenLine = linePoints [i];
-						tempFlattenLine.y = m_pointCloud.m_floorPlaneY;//m_pointCloudFloor.transform.position.y;
-						Debug.Log ("YYYY " + m_pointCloud.m_floorPlaneY);
-						linePoints [i] = tempFlattenLine;
-					}
-
-			}
-		}
-	}
 
     /// <summary>
     /// Wait for the next depth update, then find the nearest point in the point
@@ -248,7 +224,6 @@ public class PointToPointGUIController : MonoBehaviour, ITangoDepth
     /// <returns>Coroutine IEnumerator.</returns>
     private IEnumerator _WaitForDepth(Vector2 touchPosition)
     {
-		
         m_waitingForDepth = true;
 
         // Turn on the camera and wait for a single depth update
@@ -263,20 +238,84 @@ public class PointToPointGUIController : MonoBehaviour, ITangoDepth
             TangoEnums.TangoDepthCameraRate.DISABLED);
 
         Camera cam = Camera.main;
-        int pointIndex = m_pointCloud.FindClosestPoint(cam, touchPosition, 1000);
+        int pointIndex = m_pointCloud.FindClosestPoint(cam, touchPosition, 10);
 
-		if (pointIndex > -1 && linePoints.Count < 5)
-        {
-			Debug.Log("WAIT FOR DEPTH." + touchPosition);
-            // Index is valid
-            //m_startPoint = m_endPoint;
-            //m_endPoint = m_pointCloud.m_points[pointIndex];
-			linePoints.Add(m_pointCloud.m_points[pointIndex]);
+        if (pointIndex > -1)
+        {   // Index is valid
+            if (screenTaps == 0) {
+                m_point0 = m_pointCloud.m_points [pointIndex];
+                Debug.Log (m_point0);
 
-            //m_distance = Vector3.Distance(m_startPoint, m_endPoint);
-			if (linePoints.Count == 4) {
-				enableLineDrawing = false;
-			}
-        } 
+            } else if (screenTaps == 1) {
+                m_point1 = m_pointCloud.m_points [pointIndex];
+                Debug.Log (m_point1);
+
+
+            } else if (screenTaps == 2) {
+                m_point2 = m_pointCloud.m_points [pointIndex];
+                Debug.Log (m_point2);
+
+            } else  {
+                m_point3 = m_pointCloud.m_points [pointIndex];
+                Debug.Log (m_point3);
+                m_distance23 = Vector3.Distance(m_point2, m_point3);
+
+                createCube (m_point0, m_distance01, m_distance23, m_distance12);
+
+            }
+
+
+        }
+        //    m_startPoint = m_endPoint;
+        //  m_point1 = m_pointCloud.m_points [pointIndex];
+        //     m_endPoint = m_pointCloud.m_points[pointIndex];
+
+        m_distance12 = Vector3.Distance(m_point1, m_point2);
+        m_distance01 = Vector3.Distance(m_point0, m_point1);
+
+
+        //  m_distance34 = Vector3.Distance(m_point3, m_point4);
+
+
+
+
+
+
+
+
+        screenTaps = screenTaps + 1;
+        Debug.Log (screenTaps);
+
+
+
+        //  insertModel (m_point0, "modern_armchair");
+
     }
+
+
+
+
+    // Model insertion script
+
+    private void insertModel(Vector3 startPos, string modelName){
+        GameObject myModel= Instantiate(Resources.Load(modelName, typeof(GameObject))) as GameObject;
+        myModel.transform.localPosition = startPos;
+        Debug.Log ("The model should be active now. Should be...");
+    }
+
+
+    private void createCube(Vector3 startPos, float length, float width, float height) {
+        //GameObject Cube = GameObject.CreatePrimitive (PrimitiveType.Cube);
+        GameObject Cube = Instantiate(Resources.Load("MeasureCube", typeof(GameObject))) as GameObject;
+        //Cube.GetComponent<Renderer> ().material.color = new Color (0.5f, 0f, 0f, 0.2f);
+        Cube.transform.position = startPos;
+        Vector3 scale = new Vector3(length, height,width);
+        Cube.transform.localScale = scale;
+
+
+    }
+
+
+
+
 }
